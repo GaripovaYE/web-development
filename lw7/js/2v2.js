42987a0ca586de5debaf63b21971f8037cf78ae3
julia.garipova <garipovaye@mail.ru>
2020-04-05 15:21:55 +0300

    Лабораторная работа 7

--- lw7/js/2v2.js
+++ lw7/js/2v2.js
@@ -1,72 +1,75 @@
 //calculateBinaryExp(str) вычисляет результат выражения формата 'операция число1 число2'
 function Calculate(symb, x, y){
-	switch(symb){
-		case '+':
-			return x + y;
-		case '-':
-			return x - y;
-		case '*':
-			return x * y;
-		case '/':
-			return x / y;	
-	}
+  switch(symb){
+    case '+':
+      return x + y;
+    case '-':
+      return x - y;
+    case '*':
+      return x * y;
+    case '/':
+      if (y != 0){
+        return x / y
+      } else {	
+        console.log('Divide by zero');
+        return NaN
+      }
+  }
 }
 
 function isNumber(x){
-	let isNum  = isFinite(x)
-	if ((isNum) && (Number(x) == 0) && (!x.includes('0'))){
-		isNum = false
-	}
-	return isNum
+  let isNum  = isFinite(x);
+  if ((isNum) && (Number(x) == 0) && (!x.includes('0'))){
+    isNum = false;
+  }
+  return isNum
 }
 
 function calculateBinaryExp(str){ 
-	const operations = '*/+-'	
-	let a, b	
-	let err
-	let oper = str[0]	
-	if (operations.includes(oper) && (str.length > 4)){
-		str = str.substr(2, str.length)
-		let posSpace = str.indexOf(' ')
-		a = str.substring(0, posSpace)
-		b = str.substr(posSpace	, str.length)
-		if (isNumber(a) && isNumber(b)){
-			return Calculate(oper, Number(a), Number(b))
-		} else {
-			err = true
-		}			
-	} else {
-		err = true
-	}
-	if (err){
-		return NaN
-	}
-}
-function simplifyExpression(s){		
-	if (s.includes(')')){
-		let i = s.indexOf(')')
-		while (s[i] != '('){
-			i--
-		}
-		let sub = s.substring(i, s.indexOf(')') + 1)
-		s = s.replace(sub, calculateBinaryExp(sub.substring(1, sub.length - 1)))
-		//s.replace(sub, calculateBinaryExp(sub.substring(1, sub.length - 1)))
-		console.log('1 '+calculateBinaryExp(sub.substring(1, sub.length - 1)))
-		console.log('s='+s + ' sub='+sub)
-		return s
-	} else {
-		return NaN
-	}
+  const operations = '*/+-';	
+  let a, b;	
+  let err;
+  let oper = str[0];
+	
+ if (operations.includes(oper) && (str.length > 4)){
+    str = str.substr(2, str.length);
+    let posSpace = str.indexOf(' ');
+    a = str.substring(0, posSpace);
+    b = str.substr(posSpace	, str.length);
+    if (isNumber(a) && isNumber(b)){
+      return Calculate(oper, Number(a), Number(b));
+    } else {
+      err = true;
+    }			
+  } else {
+    err = true;
+  }
+  if (err){
+    return NaN
+  }
 }
 
-function calc(s){
-	//while (s.includes('(')){
-	if (s.includes('(')){
-		s = simplifyExpression(s)
-		console.log('3' + s)
-	}
-	console.log(s)
-	return calculateBinaryExp(s)
+function simplifyExpression(s){		
+  if (s.includes(')')){
+    let i = s.indexOf(')')
+    while (s[i] != '('){
+      i--;
+    }
+    let sub = s.substring(i, s.indexOf(')') + 1);
+    s = s.replace(sub, calculateBinaryExp(sub.substring(1, sub.length - 1)));
+    return s
+  } else {
+    return NaN
+  }
 }
-let str = "((/ 4  35 ))"
-console.log(calc(str))
+
+function calc(s){ 
+  while (s.includes('(')){
+    s = simplifyExpression(s);
+  }
+  return calculateBinaryExp(s)
+}
+
+let str = "+ (/ 4  0 ) (* 4 (+ 1 1))";
+
+console.log(calc(str));
