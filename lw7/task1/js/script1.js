233370d4601e719d20a7f7c73744fa48fdb211b6
julia.garipova <garipovaye@mail.ru>
2020-04-19 15:55:39 +0300

    Лабораторная работа 7

---
+++ lw7/task1/js/script1.js
@@ -0,0 +1,48 @@
+function isPrimeNum(num){
+  if (Number.isInteger(num) && num > 1){ 
+    let isPrime = true;
+    for (let j = 2; j < num; j++){
+      if (num % j == 0){
+        isPrime = false;
+        break;
+      }
+    }
+    return isPrime
+  } else {
+    return false
+  }
+}
+
+function isValid(numb){
+  return (typeof numb == "number")
+}
+
+function isValidData(data){
+  return (data == 'number') || Array.isArray(data)
+}
+
+function isPrimeNumber(num){  
+  if (isValidData(num)){
+    let numArray;
+    if (typeof num == "number"){
+      numArray = [num];
+    } else {
+      numArray = num;
+    }    
+    for (let i = 0; i < numArray.length; i++){
+      if (isValid(numArray[i])){
+	(isPrimeNum(numArray[i]))
+          ? console.log(numArray[i] + ' is prime number')
+          : console.log(numArray[i] + ' is not prime number')
+      }
+    } 
+  }
+}
+
+// let n = [5, '5', 20];
+// let n = 5;
+// let n;
+let n = [5, 20];
+
+isPrimeNumber(n);
+
