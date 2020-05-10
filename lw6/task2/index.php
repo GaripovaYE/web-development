7a8f5249c3bc09149b8f425b0eb6ccee3b90914e
julia.garipova <garipovaye@mail.ru>
2020-05-10 12:09:33 +0300

    Лабораторная работа 6

--- lw6/task2/index.php
+++ lw6/task2/index.php
@@ -110,30 +110,7 @@
     </div>
     <div class="form_block">
       <h2 class="form_title">НАПИШИ МНЕ</h2>
-      <form class="form_fields" method="GET" action="../src/save_survey_V2.php" enctype="encodingType">
-        <label class="required_field_name" for="name">Ваше имя</label>
-        <input required class="text_field" type="text" name="name" id="name">          
-        <label class="required_field_name" for="email">Ваш email</label>
-        <input required class="text_field" type="text" name="email" id="email">
-        <label class="field_name" for="country">Откуда Вы?</label>
-        <select class="select_country" name="country" id="country">
-          <option>Белоруссия</option>
-          <option>Грузия</option>
-          <option>Монголия</option>		  
-          <option selected>Россия</option>
-          <option>Украина</option>
-        </select>
-        <div>  
-          <label class="radio_name" for="gender">Ваш пол</br></label>
-          <input class="radio_checker" type="radio" name="gender" id="gender" value="male" checked>
-          <span class="radio_description">Мужской</span>
-          <input class="radio_checker" type="radio" name="gender" id="gender" value="female">
-          <span class="radio_description">Женский</span>
-        </div>
-        <div class="message_field_name">Ваше сообщение</div>
-        <textarea required name="message"></textarea>
-        <input type="submit" class="button_send" value="Отправить">
-      </form>	
+	    <? include 'form.php' ?>
     </div>
     <footer class="footer_decoration">
       &#169; 2006-2018 Поволжский государственный технологический университет, ФГБОУ ВО «ПГТУ»
